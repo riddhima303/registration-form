fb3e7b45e86b093341300e40fdddc98f10989846
Riddhima Gupta <riddhima303@gmail.com>
2026-07-10 02:52:59 +0530

    Fix QR code URL

--- success.js
+++ success.js
@@ -1,18 +1,16 @@
 // Get the ID from the URL
 const params = new URLSearchParams(window.location.search);
-
 const id = params.get("id");
 
 // Show the registration ID
 document.getElementById("registrationId").textContent = id;
 
-// Generate the QR Code
+// Generate QR Code
 const verifyUrl =
-`https://registration-page01.pages.dev/verify.html?id=${id}`;
+`https://registration-form-flame-theta.vercel.app/verify.html?id=${id}`;
 
 document.getElementById("qrImage").src =
 `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${encodeURIComponent(verifyUrl)}`;
 
-
-qrImage.width = 250;
-qrImage.height = 250;
+document.getElementById("qrImage").width = 250;
+document.getElementById("qrImage").height = 250;
