afed8e8814e9824cb5cc084486cc98ef08d0fe85
Riddhima Gupta <riddhima303@gmail.com>
2026-07-10 11:17:32 +0530

    duplicate

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const form = document.getElementById("registrationForm");
 const SCRIPT_URL =
 "https://script.google.com/macros/s/AKfycbyc1b1c88Y-BuDVGj_AilWYgkmEgmxyAITKrH8Hd7n9D2nf7M9KtnSXxfEbJsmzaTSXyw/exec";
 
+const message = document.getElementById("message");
+
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
+
+    message.textContent = "";
 
     const data = {
         name: document.getElementById("name").value,
@@ -21,15 +25,20 @@ form.addEventListener("submit", async (e) => {
         });
 
         const result = await response.json();
+
         if (result.status === "duplicate") {
-        alert("This phone number is already registered.");
-        return;
-}
+
+            message.textContent = "❌ This phone number is already registered.";
+            message.className = "error";
+            return;
+        }
 
         window.location.href = `success.html?id=${result.id}`;
 
     } catch (error) {
         console.error(error);
-        alert("Registration failed.");
+
+        message.textContent = "❌ Registration failed. Please try again.";
+        message.className = "error";
     }
 });
