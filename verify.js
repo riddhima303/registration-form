22c73bfcc0bb18534bcdb566a104796db2036b6f
Riddhima Gupta <riddhima303@gmail.com>
2026-07-10 13:52:40 +0530

    Rename success video

--- verify.js
+++ verify.js
@@ -1,60 +1,75 @@
 console.log("verify.js loaded");
 
+// Get ID from URL
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
+// Google Apps Script URL
 const SCRIPT_URL =
 "https://script.google.com/macros/s/AKfycbyc1b1c88Y-BuDVGj_AilWYgkmEgmxyAITKrH8Hd7n9D2nf7M9KtnSXxfEbJsmzaTSXyw/exec?id=" + id;
 
+// Verify Ticket
 fetch(SCRIPT_URL)
-  .then(res => res.json())
+  .then(response => response.json())
   .then(data => {
+
+    const result = document.getElementById("result");
+
     if (data.status === "valid") {
 
-  document.getElementById("result").innerHTML = `
-    <div class="ticket valid">
-      <video
-        autoplay
-        muted
-        playsinline
-        width="120"
-        height="120">
-        <source src="./success-check.mp4" type="video/mp4">
-      </video>
+      result.innerHTML = `
+        <div class="ticket valid">
 
-      <h1>VERIFIED</h1>
-    </div>
-  `;
+          <video autoplay muted playsinline width="140">
+            <source src="success-check.mp4" type="video/mp4">
+          </video>
 
-}
-    else if (data.status === "used") {
+          <h1>✅ VERIFIED</h1>
 
-      document.getElementById("result").innerHTML = `
+          <p>Access Granted</p>
+
+        </div>
+      `;
+
+    } else if (data.status === "used") {
+
+      result.innerHTML = `
         <div class="ticket used">
+
           <h1>⚠️ TICKET ALREADY USED</h1>
-          <p>This QR code has already been scanned.</p>
+
+          <p>This QR Code has already been scanned.</p>
+
         </div>
       `;
 
     } else {
 
-      document.getElementById("result").innerHTML = `
+      result.innerHTML = `
         <div class="ticket invalid">
+
           <h1>❌ INVALID TICKET</h1>
+
           <p>Entry Denied</p>
+
         </div>
       `;
 
     }
 
   })
-  .catch(err => {
-    console.error(err);
+  .catch(error => {
+
+    console.error(error);
 
     document.getElementById("result").innerHTML = `
       <div class="ticket invalid">
+
         <h1>⚠️ Error</h1>
+
         <p>Unable to verify ticket.</p>
+
       </div>
     `;
+
   });
